ff9611a195416e4b9c10f06ebc18b8a4d20fb13b
Charalampos Karypidis <xaralaboskaripidis@gmail.com>
2015-12-12 21:14:47 +0100

    Add a component with state to demonstrate the live reload of react component without loosing the state

--- samples/AspnetReact.Webpack/reactApp/components/app.jsx
+++ samples/AspnetReact.Webpack/reactApp/components/app.jsx
@@ -2,14 +2,17 @@ import React, {Component} from 'react';
 
 import '../styles/site.scss';
 
+import Counter from './counter.jsx';
+
+
 class App extends Component {
-	render() {
-		return (
-			<div>
-				this is the first result
-			</div>
-		);
-	}
+  render() {
+    return (
+	  <div>
+	    <Counter />
+	  </div>
+    );
+  }
 }
 
 export default App;
